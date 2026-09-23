6a1f1e69100e99c8e825f7f5d4b9696fc776b7b1
aukvary <al3xpop123@gmail.com>
2026-09-23 15:07:02 +0700

    task 1 2

--- Dolgikh Aleksei/task2/main.c
+++ Dolgikh Aleksei/task2/main.c
@@ -12,8 +12,20 @@ int main(void) {
     struct tm* sp = localtime(&now);
 
     printf(
-        "%02d/%02d/%04d %02d:%02d %s\n", sp->tm_mday, sp->tm_mon + 1,
-        sp->tm_year + 1900, sp->tm_hour, sp->tm_min, tzname[sp->tm_isdst]
+        "%02d/%02d/%04d %02d:%02d:%02d %s\n", sp->tm_mday, sp->tm_mon + 1,
+        sp->tm_year + 1900, sp->tm_hour, sp->tm_min, sp->tm_sec,
+        tzname[sp->tm_isdst]
+    );
+
+    setenv("TZ", "Europe/Moscow", 1);
+    time(&now);
+
+    sp = localtime(&now);
+
+    printf(
+        "%02d/%02d/%04d %02d:%02d:%02d %s\n", sp->tm_mday, sp->tm_mon + 1,
+        sp->tm_year + 1900, sp->tm_hour, sp->tm_min, sp->tm_sec,
+        tzname[sp->tm_isdst]
     );
 
     return 0;
